450df8eedb0d5a8348c579c2495aa491913fa722
pablo <pablo_mix_16@hotmail.com>
2025-10-03 14:35:15 -0300

    chore: setup frontend for Vercel SPA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "tailwindcss";
+// import * as path from "path";
 
-// https://vite.dev/config/
+// Use Node.js __dirname directly in Vite config
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname,
+    },
+  },
+  base: "/", // importante para rutas absolutas
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
 });
