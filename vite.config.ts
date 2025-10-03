3a1b5cfc9a87cadc2f34520e6a53b0050c4ef46b
pablo <pablo_mix_16@hotmail.com>
2025-10-03 15:29:27 -0300

    dasd

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // FIX: Establecer la ruta base a la raíz del dominio para un despliegue correcto en Vercel
+  base: "/",
+
   plugins: [react(), tailwindcss()],
 });
